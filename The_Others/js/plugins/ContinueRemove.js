41584b36e7ce7f108a740c7ba4bc7f81b2bd285a
D-e-elle <84299963+D-e-elle@users.noreply.github.com>
2022-10-22 19:57:32 -0600

    PowerRoom Complete

--- The_Others/js/plugins/ContinueRemove.js
+++
@@ -1,5 +0,0 @@
-WindowTitleCommand.prototype.makeCommandList = function() {
-    this.addCommand(TextManager.newGame,   'newGame');
-//    this.addCommand(TextManager.continue, 'continue', this.isContinueEnabled());
-    this.addCommand(TextManager.options,   'options');
-};
